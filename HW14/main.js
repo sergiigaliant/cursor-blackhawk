f85d6c43ce4519e18679c41aed73063bc85d424f
sergiigaliant <galyant89@gmail.com>
2020-06-25 19:54:55 +0300

    video optimization

--- HW14/main.js
+++ HW14/main.js
@@ -1,4 +1,12 @@
 $(window).on("load", () => {
   console.log("on load");
-  $(".preloader").fadeOut(500);
+    $(".preloader").fadeOut(1000);
+    $(".video-container").append(`<iframe
+            width = "780"
+            height = "380"
+            src = "https://www.youtube.com/embed/M7lc1UVf-VE"
+            frameborder = "0"
+            allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe >`)
 });
