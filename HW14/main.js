3f1c12174a182f9610a1683479481194a9b70b05
sergiigaliant <galyant89@gmail.com>
2020-06-26 11:04:20 +0300

    added slider slick

--- HW14/main.js
+++ HW14/main.js
@@ -1,12 +1,11 @@
 $(window).on("load", () => {
   console.log("on load");
     $(".preloader").fadeOut(1000);
-    $(".video-container").append(`<iframe
-            width = "780"
-            height = "380"
-            src = "https://www.youtube.com/embed/M7lc1UVf-VE"
-            frameborder = "0"
-            allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-        ></iframe >`)
+       
+});
+$(document).ready(function () {
+    $('.review-slider').slick({
+        prevArrow: $(".arrow-left"),
+        nextArrow: $(".arrow-right")
+    });
 });
